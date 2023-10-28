8c8df1ee4ebe04c640b79f27187bde1f1b4353c8
Sumanth Angadi <91518618+angadisumanth10@users.noreply.github.com>
2023-10-28 20:36:23 +0530

    Update url-shortener.py
    Code Completed..

--- url-shortener.py
+++ url-shortener.py
@@ -6,7 +6,9 @@ import pyshorteners
 url = input('Enter the Url: ')
 
 # Define the function..
-
+def shortenurl(url):
+    s = pyshorteners.Shortener()
+    print(s.tinyurl.short(url))
 
 shortenurl(url)
 
